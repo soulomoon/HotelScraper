aacbc20a91fe7594be2afbedd7e583b79d8f0e86
soulomoon <fwy996602672@gmail.com>
2017-12-11 18:29:34 +0800

    resolve conflict names

--- manage.py
+++ manage.py
@@ -4,26 +4,27 @@ import parser
 from validate import validate_date
 
 
-parser = argparse.ArgumentParser(description='scraping airbnb and booking')
-parser.add_argument(
+#
+ps = argparse.ArgumentParser(description='scraping airbnb and booking')
+ps.add_argument(
     'destination',
     metavar='DESTINATION',
     type=str,
     nargs=None,
     help='destination')
-parser.add_argument(
+ps.add_argument(
     'checkin',
     metavar='YYYY-MM-DD',
     type=str,
     nargs=None,
     help='checkin date: YYYY-MM-DD')
-parser.add_argument(
+ps.add_argument(
     'checkout',
     metavar='YYYY-MM-DD',
     type=str,
     nargs=None,
     help='checkout date: YYYY-MM-DD')
-args = parser.parse_args()
+args = ps.parse_args()
 
 
 if __name__ == "__main__":
