f304c2547ee3ac7f4c1e2a06411293e4db08fd80
soulomoon <fwy996602672@gmail.com>
2017-12-11 12:51:18 +0800

    add AirbnbScraper

--- app.py
+++ app.py
@@ -3,51 +3,65 @@ from abc import ABC, abstractproperty
 from selenium import webdriver
 from urllib.parse import urlencode
 from bs4 import BeautifulSoup
-from parser import *
-from cssname import SPECIAL_URL
-from urltojson import url_to_dict
-
-PARAMS_DICT = {
-    "checkin_year_month_monthday": "2017-12-14",
-    "checkout_year_month_monthday": "2017-12-15",
-    "ss": "guangzhou",
-    "nflt": "class=4;class=3;class=5;",
-    "rows": "40",
-    "offset": "0",
-    "selected_currency": "HKD"
-}
-
-SPECIAL_DICT = PARAMS_DICT.copy()
-SPECIAL_DICT.update({"nflt": "class=4;class=3;class=5;fc=2;mealplan=1;hotelfacility=16;hotelfacility=107;hotelfacility=28;popular_activities=11;"})
+from urllib.parse import parse_qs, urlparse, urlencode
+from names import BOOKING_SPECIAL_DICT, BOOKING_QUERY_DICT, AIRBNB_QUERY_DICT
+from data import *
 
 class PagingScraper(ABC):
     def __init__(self, *args, **kwargs):
+        self._url = self.url_search + urlencode(self.init_query_dict)
         self.source_list = []
-        self._url = self.url_search + urlencode(self.params_dict)
+        self.counter = 0
 
     def __enter__(self):
         self.browser = webdriver.Firefox()
+        print("loading begin")
         self.collect()
-        input()
         return self
 
     def __exit__(self, type, value, traceback):
         self.browser.close()
 
     def collect(self):
+        print("loading: {}".format(self._url))
         self.browser.get(self._url)
-        self.source_list.append(self.browser.page_source)
+        #self.source_list.append(self.browser.page_source)
+        if self.counter == 0:
+            delete_file(self.__class__.__name__)
+        save_file(self.__class__.__name__+str(self.counter), self.browser.page_source)
+        print("fetching data done")
+        self.counter += 1
 
-    def update(self, prprty, value):
-        self.params_dict.update([(prprty, value)])
+    def update_url(self, prprty, value):
+        """update_url
+
+        :param prprty:
+        :param value:
+        """
+        #update query
+        parsed = urlparse(self._url)
+        query_dict = parse_qs(parsed.query)
+        query_dict.update([(prprty, [value])])
+        query=urlencode(query_dict, doseq=True)
+        #parse url and replace querystring
+        parsed = parsed._replace(query=query)
+        print(parsed.geturl())
+        self._url = parsed.geturl()
 
     def set_next_url(self):
-        return None
+        print("finding next url")
         soup = BeautifulSoup(self.browser.page_source, "html.parser")
         url_rest = self.fetch_next_url(soup)
         if url_rest and url_rest.has_attr('href'):
             self._url = self.url_home + url_rest['href']
+            print("next url found:{}".format(self._url))
             return self._url
+        print("next url not found for: {}".format(self._url))
+
+    def get_all_pages(self):
+        while self.set_next_url():
+            self.collect()
+        return self.source_list
 
     @abstractproperty
     def fetch_next_url(self, soup):
@@ -62,29 +76,8 @@ class PagingScraper(ABC):
         pass
 
     @abstractproperty
-    def params_dict(self):
+    def init_query_dict(self):
         pass
-
-    @abstractproperty
-    def get_init_dict(self):
-        pass
-
-    @property
-    def params_dict(self):
-        if not hasattr(self, "_params_dict"):
-            self._params_dict =  self.get_init_dict
-        return self._params_dict
-
-    @params_dict.setter
-    def params_dict(self, value):
-        self._params_dict = value
-
-    def get_all_pages(self):
-        while self.set_next_url():
-            self.collect()
-        return self.source_list
-
-
 
 class BookingScraper(PagingScraper):
     def __init__(self, place, bdate, edate):
@@ -94,13 +87,13 @@ class BookingScraper(PagingScraper):
         :param edate: 20xx-xx-xx
         """
         super().__init__()
-        self.update("ss", place)
-        self.update("checkin_year_month_monthday", bdate)
-        self.update("checkout_year_month_monthday", edate)
+        self.update_url("ss", place)
+        self.update_url("checkin_year_month_monthday", bdate)
+        self.update_url("checkout_year_month_monthday", edate)
 
     @property
-    def get_init_dict(self):
-        return PARAMS_DICT.copy()
+    def init_query_dict(self):
+        return BOOKING_QUERY_DICT.copy()
 
     @property
     def url_home(self):
@@ -116,23 +109,54 @@ class BookingScraper(PagingScraper):
 
 class SpecialBookingScraper(BookingScraper):
     @property
-    def get_init_dict(self):
-        return SPECIAL_DICT.copy()
+    def init_query_dict(self):
+        return BOOKING_SPECIAL_DICT.copy()
+
+
+class AirbnbScraper(PagingScraper):
+    def __init__(self, place, bdate, edate):
+        """__init__
+        :param place:
+        :param bdate: 20xx-xx-xx
+        :param edate: 20xx-xx-xx
+        """
+        self.place = place
+        super().__init__()
+        print(self._url)
+        self.update_url("checkin", bdate)
+        self.update_url("checkout", edate)
+        print(self._url)
+
+    @property
+    def init_query_dict(self):
+        return AIRBNB_QUERY_DICT.copy()
+
+    @property
+    def url_home(self):
+        return "https://www.airbnb.com"
+
+    @property
+    def url_search(self):
+        return "{home}/s/{place}/homes?".format(home=self.url_home, place=self.place)
+
+    def fetch_next_url(self, soup):
+        li = soup.find("li", class_="_b8vexar")
+        if li:
+            return li.find("a", class_="_1ko8une")
+
 
 def get_all_pages(scraper, place, indate, outdate):
     with scraper(place, indate, outdate) as br:
         return br.get_all_pages()
     raise "scraper({}): scraping make a mistake, please restart".format(type(scraper).__name__)
 
+
 def run(place, indate, outdate):
-    all_pages = get_all_pages(SpecialBookingScraper, place, indate, outdate)
+    airbnb_all_pages = get_all_pages(AirbnbScraper, place, indate, outdate)
+    all_pages = get_all_pages(BookingScraper, place, indate, outdate)
     all_special_pages = get_all_pages(SpecialBookingScraper, place, indate, outdate)
 
 
-
 if __name__ == "__main__":
-    run('guangzhou', '2018-05-03', '2018-05-04')
+    run('guangzhou', '2017-12-19', '2017-12-20')
 
-    textfile = open('textfile.txt', 'w', encoding='utf-8')
-    textfile.write(all_pages[0])
-    textfile.close()
