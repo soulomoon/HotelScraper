be2627fd02921e72e65146141e35fc899726fda9
soulomoon <fwy996602672@gmail.com>
2017-12-11 12:53:43 +0800

    rearragement

---
+++ __init__.py
@@ -0,0 +1,6 @@
+import app
+import parser
+
+if __name__ == "__main__":
+    app.run('guangzhou', '2018-05-03', '2018-05-04')
+    parser.run()
--- common.py
+++ common.py
@@ -1,6 +1,12 @@
-from urllib.parse import parse_qs
-
-def url_to_json(url):
-    params_dict = parse_qs(url)
+import re
 
 
+def price_filter(original_function):
+    def wrapper(*args, **kwargs):
+        prcstr = original_function(*args, **kwargs)
+        if not prcstr:
+            return None
+        prcstr = prcstr.replace(",", "")
+        reg = re.search(r"\d+", prcstr)
+        return reg.group() if reg else None
+    return wrapper
